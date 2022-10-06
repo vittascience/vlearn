9c17a385ac8fc7b8f16d0d08dac1a908bf8a7229
Arouz <48770524+Arouz@users.noreply.github.com>
2022-10-06 17:58:42 +0200

    Update ControllerCourseLinkUser.php

--- Controller/ControllerCourseLinkUser.php
+++ Controller/ControllerCourseLinkUser.php
@@ -3,6 +3,10 @@
 namespace Learn\Controller;
 
 use Learn\Entity\Course;
+use Learn\Entity\Activity;
+use Learn\Entity\CourseLinkActivity;
+use Classroom\Entity\ActivityLinkUser;
+use Classroom\Entity\ClassroomLinkUser;
 
 class ControllerCourseLinkUser extends Controller
 {
@@ -12,7 +16,57 @@ class ControllerCourseLinkUser extends Controller
         $this->actions = array(
             'get_my_courses_as_teacher' => function () {
                 $arrayCoursesResult = $this->entityManager->getRepository(Course::class)->findBy(['user' => $this->user]);
-                return  $arrayCoursesResult;
+                $arrayResult = [];
+
+                foreach ($arrayCoursesResult as $courses) {
+                    $myCourse = $courses->jsonSerialize();
+                    $courseLinkActivity = $this->entityManager->getRepository(CourseLinkActivity::class)->findBy(['course' => $myCourse['id']]);
+                    $myActivities = [];
+                    foreach ($courseLinkActivity as $link) {
+                        $activity = $this->entityManager->getRepository(Activity::class)->findOneBy(['id' => $link->getActivity()->getId()]);
+                        $myActivities[] = $activity->jsonSerialize();
+                    }
+                    $myCourse['activities'] = $myActivities;
+                    array_push($arrayResult, $myCourse);
+                }
+
+                return  $arrayResult;
+            },
+
+            'link_user_to_course' => function () {
+
+                $courseId = htmlspecialchars($_POST['courseId']);
+                $classroomId = $_POST['classrooms'];
+                $students = $_POST['students'];
+
+
+                foreach ($classroomId as $classId) {
+                    $teacher = $this->entityManager->getRepository(ClassroomLinkUser::class)->findOneBy(['user' => $_SESSION['id'], 'classroom' => $classId, 'rights' => 2]);
+                    if (!$teacher) {
+                        return ["success" => false, "message" => "Vous n'êtes pas professeur de cette classe"];
+                    }
+                }
+
+                // get the course 
+                $course = $this->entityManager->getRepository(Course::class)->findOneBy(['id' => $courseId]);
+                // get the activities of the course
+                $courseLinkActivity = $this->entityManager->getRepository(CourseLinkActivity::class)->findBy(['course' => $courseId]);
+
+                // for each student
+                foreach ($students as $studentId) {
+                    foreach ($courseLinkActivity as $link) {
+
+                        $activity = $this->entityManager->getRepository(Activity::class)->findOneBy(['id' => $link->getActivity()->getId()]);
+                        $user = $this->entityManager->getRepository(User::class)->findOneBy(['id' => $studentId]);
+                        $linkActivityToUser = new ActivityLinkUser($activity, $user, new \DateTime(), null);
+                        $linkActivityToUser->is
+                    }
+                }
+
+
+
+
+                return true;
             },
         );
     }
